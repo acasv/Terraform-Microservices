a061477e82285dd7a91745e8ada1b4e77623bea3
acasv <andres.castrillonv@perficient.com>
2022-10-04 18:35:57 -0500

    jenkins installation script

--- infra/frontend.sh
+++ infra/frontend.sh
@@ -2,7 +2,26 @@
 
 HOME='/home/ubuntu'
 
-sudo apt-get update
+apt update
+
+echo "---------------------------------Installing java---------------------------------"
+sudo apt install openjdk-11-jre-headless -y
+
+echo "---------------------------------Installing Jenkins---------------------------------"
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update
+sudo apt-get install jenkins -y
+
+sudo sed -i 's/HTTP_PORT=8080/HTTP_PORT=8090/g' /etc/default/jenkins
+
+echo "---------------------------------Enable Jenkins---------------------------------"
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo service jenkins restart
 
 echo "---------------------------------Installing Node---------------------------------"
 curl -fsSL https://deb.nodesource.com/setup_8.x | sudo -E bash -
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,4 @@
 output "public_ip_address" {
   description = "public ip address"
-  value       = aws_instance.microserviceFront.public_ip
-}
-output "public_ip_address" {
-  description = "public ip address"
-  value       = aws_instance.microserviceFront2.public_ip
+  value       = [aws_instance.microserviceFront.public_ip,aws_instance.microserviceFront2.public_ip]
 }
